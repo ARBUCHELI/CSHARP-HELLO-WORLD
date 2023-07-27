81fbbf7bbee8f0722d6718d60f9594db7384b59e
ARBUCHELI <radio_seattle@hotmail.com>
2023-07-27 20:03:06 +0300

    first commit

---
+++ hello-world-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Hello World from this edited code!");    
+     }
+  }
+}
+
+/* In order to run these kinds of programs, go the the folder that contains the program and follow the steps:
+    1) type cmd
+    2) In cmd type >> dotnet new console
+    3) That will generate a Program.cs file.  Edit that file with your code (For now.  In the future we'll find a more efficient
+    way to do this.)
+    4) In the terminal type dotnet run
+*/
+ 
