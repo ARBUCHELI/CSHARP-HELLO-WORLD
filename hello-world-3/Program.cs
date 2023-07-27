227cdda4bc70ae2663f8a95a3e4e99eb22555fbb
ARBUCHELI <radio_seattle@hotmail.com>
2023-07-27 21:09:48 +0300

    getting input

---
+++ hello-world-3/Program.cs
@@ -0,0 +1,22 @@
+﻿/* In order to run these kinds of programs, go the the folder that contains the program and follow the steps:
+    1) type cmd
+    2) In cmd type >> dotnet new console
+    3) That will generate a Program.cs file.  Edit that file with your code (For now.  In the future we'll find a more efficient
+    way to do this.)
+    4) In the terminal type dotnet run
+*/
+
+using System;
+
+namespace GettingInput
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("How old are you?");
+      string input = Console.ReadLine();
+      Console.WriteLine($"You are {input} years old!");
+    }
+  }
+}
