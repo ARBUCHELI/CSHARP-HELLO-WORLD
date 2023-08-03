adf7d1fae7b279789b20aefa119b083b3a4ab7b6
ARBUCHELI <radio_seattle@hotmail.com>
2023-08-03 10:05:48 +0300

    project console creatures

--- hello-world-3/Program.cs
+++ hello-world-3/Program.cs
@@ -1,22 +1,2 @@
-﻿/* In order to run these kinds of programs, go the the folder that contains the program and follow the steps:
-    1) type cmd
-    2) In cmd type >> dotnet new console
-    3) That will generate a Program.cs file.  Edit that file with your code (For now.  In the future we'll find a more efficient
-    way to do this.)
-    4) In the terminal type dotnet run
-*/
-
-using System;
-
-namespace GettingInput
-{
-  class Program
-  {
-    static void Main()
-    {
-      Console.WriteLine("How old are you?");
-      string input = Console.ReadLine();
-      Console.WriteLine($"You are {input} years old!");
-    }
-  }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ project-console-creatures/Program.cs
@@ -0,0 +1,26 @@
+﻿/* In order to run these kinds of programs, go the the folder that contains the program and follow the steps:
+    1) type cmd
+    2) In cmd type >> dotnet new console
+    3) That will generate a Program.cs file.  Edit that file with your code (For now.  In the future we'll find a more efficient
+    way to do this.)
+    4) In the terminal type dotnet run
+*/
+
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      /* The purpose of this program is to print C# Creatures*/
+      Console.WriteLine(" .-.");   
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Spiro the Specter");
+     }
+  }
+}
